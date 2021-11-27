02e8cccc560e5e38889e3f6af125f9ae2e5ce2e3
Rutvik Padhiyar <rutvikpadhiyar000@gmail.com>
2021-11-27 12:54:32 +0530

    Used mysql database for backend

--- project/settings.py
+++ project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'atgdata',
+        'USER': 'rutvik',
+        'PASSWORD': 'Lk_7>3}.',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
