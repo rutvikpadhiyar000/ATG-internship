8446ea7298be11c452e36ecff184f57acd200cbe
Rutvik Padhiyar <rutvikpadhiyar000@gmail.com>
2021-11-30 21:14:35 +0530

    created 'appointment' model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='patient')
+    doctor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='doctor')
+
+    req_speciality = models.CharField(max_length=15)
+    date = models.DateField()
+    start = models.TimeField()
+    end = models.TimeField()
