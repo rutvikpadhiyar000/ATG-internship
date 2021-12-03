56e4044b4d9a11c0397d43ea87a6ab919985d6df
rutvik padhiyar <76583071+rutvikpadhiyar000@users.noreply.github.com>
2021-12-03 10:50:08 +0530

    Added SQLite3 database

--- project/settings.py
+++ project/settings.py
@@ -94,15 +94,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'testDB',
-        'USER': 'rutvik',
-        'PASSWORD': 'Lk_7>3}.',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
